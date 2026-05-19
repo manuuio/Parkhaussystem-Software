a4c7b49f251cab6deb74da2f19e9accefc902087
Manuel D. <221882867+manuuio@users.noreply.github.com>
2026-05-19 22:55:50 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Parkhaussystem_Software
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
